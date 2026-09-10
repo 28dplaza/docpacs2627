1fdabbfbb831e2014ba51c51115bfdeeac8d00c4
28dplaza <28dplaza@live.ytech.edu>
2026-09-10 11:10:29 -0400

    worked on u1 l4

---
+++ docpac_041109/Unit1Lesson4/PlazaDahlia/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    if (condition) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('testing\n'); 
+    }
+});
+server.listen(process.env.PORT, 'localhost', () => {
+    console.log(`${process.env.APP_NAME} is running at http://localhost:${process.env.PORT}/`);
+});
