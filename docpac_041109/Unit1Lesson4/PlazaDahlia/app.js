8fe4cd9f9ca0392dc83c5dc4565c7099129b0351
28dplaza <28dplaza@live.ytech.edu>
2026-09-11 14:33:35 -0400

    worked on u1 l4

--- docpac_041109/Unit1Lesson4/PlazaDahlia/app.js
+++ docpac_041109/Unit1Lesson4/PlazaDahlia/app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
-    if (condition) {
+    if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('testing\n'); 
     }
