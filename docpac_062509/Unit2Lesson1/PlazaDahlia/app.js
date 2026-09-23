ed99798a55319a4b06debc2cb3f9651226ec7a43
28dplaza <28dplaza@live.ytech.edu>
2026-09-23 13:41:04 -0400

    Update app.js

--- docpac_062509/Unit2Lesson1/PlazaDahlia/app.js
+++ docpac_062509/Unit2Lesson1/PlazaDahlia/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
-server.listen(process.env.PORT, 'localhost', () => {
+const express = require('express')
+const app = express();
+app.listen(process.env.PORT, 'localhost', () => {
     console.log(`${process.env.APP_NAME} is running at http://localhost:${process.env.PORT}/`);
 });
