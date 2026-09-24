b2c4214887068514dc923cae53967799c4e58181
28dplaza <28dplaza@live.ytech.edu>
2026-09-24 10:10:15 -0400

    worked on u2 l1

--- docpac_062509/Unit2Lesson1/PlazaDahlia/app.js
+++ docpac_062509/Unit2Lesson1/PlazaDahlia/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express')
 const app = express();
+const path = require('path');
+app.use(express.static('public'));
 app.listen(process.env.PORT, 'localhost', () => {
     console.log(`${process.env.APP_NAME} is running at http://localhost:${process.env.PORT}/`);
 });
